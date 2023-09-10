d591dd1bd62139573faf2c8463c02623b6784981
[Felipe Larumbe] <[felipelarumbe157@gmail.com]>
2023-09-10 14:38:42 -0300

    Cambios en la clase Recipe

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections;
+using System.Linq; // brinda una serie de extensiones y operaciones que permiten realizar consultas
 
 namespace Full_GRASP_And_SOLID.Library
 {
@@ -33,6 +34,21 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+        }
+        //El principio que se uso es el SOLID : Este método sigue el principio de responsabilidad única al asignar
+        // la responsabilidad de calcular el costo total de producción a la clase Recipe.
+        public double GetProductionCost()
+        {
+             double costOfIngredients = 0;
+            double costOfEquipment = 0;
+
+            foreach (Step step in this.steps)
+            {
+                costOfIngredients += step.Quantity * step.Input.UnitCost;
+                costOfEquipment += (step.Time / 60.0) * step.Equipment.HourlyCost; // Convierte minutos a horas
+            }
+
+            return costOfIngredients + costOfEquipment;
         }
     }
 }
